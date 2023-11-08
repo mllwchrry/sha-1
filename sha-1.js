0c965f21df95416dcd1a45b71acd920646a15f2d
Mariia Zhvanko <zhvmr09@gmail.com>
2023-11-08 20:08:10 +0200

    comments added

--- sha-1.js
+++ sha-1.js
@@ -1,25 +1,39 @@
+// The main sha-1 function
 module.exports = function sha1(message) {
     return blocksToHex(sha1_rounds(pad(message)));
 }
 
+// This function takes a message and pads it to meet SHA-1 block size requirements
+// It returns an array of 32-bit blocks representing the padded message
 function pad(message) {
+    // length of the input message in chars
     const len = message.length;
+
+    // number of blocks required for padding
     const blockAmount = Math.ceil((len + 8) / 64);
+
+    // initialize an array to store 32-bit blocks, with each block containing 16 words
     const blocks = new Array(blockAmount * 16).fill(0);
 
     for (let i = 0; i < len; i++) {
+        // calculate the index of the block and word where the character should be stored
         const blockIndex = Math.floor(i / 4);
         const wordIndex = (i % 4) * 8;
+
+        // set the bits in the block to the character's ASCII code
         blocks[blockIndex] |= message.charCodeAt(i) << (24 - wordIndex);
     }
 
-    blocks[Math.floor(len / 4)] |= 0x80 << (24 - (len % 4) * 8);
+    // append 1 bit
+    blocks[Math.floor(len / 4)] |= 0x80 << (24 - (len % 4) * 8); // Add the 0x80 bit to the end of the message
 
-    blocks[blockAmount * 16 - 1] = len * 8;
+    // append the length of the original message
+    blocks[blockAmount * 16 - 1] = len * 8; // Append the message length in bits to the last block
 
     return blocks;
 }
 
+// This function converts an array of 32-bit blocks into a hexadecimal string
 function blocksToHex(blocks) {
     let hex = '';
     for (let i = 0; i < blocks.length; i++) {
@@ -35,7 +49,15 @@ function blocksToHex(blocks) {
     return hex;
 }
 
+/**
+ * Perform the SHA-1 hashing rounds on a block of data.
+ *
+ * @param {Array} block - An array of 32-bit words representing the data block.
+ * @returns {Array} An array of five 32-bit words representing the SHA-1 hash.
+ */
 function sha1_rounds(block) {
+
+    // initialize variables with initial hash values
     let w = [];
     let a = 0x67452301;
     let b = 0xEFCDAB89;
@@ -43,11 +65,16 @@ function sha1_rounds(block) {
     let d = 0x10325476;
     let e = 0xC3D2E1F0;
 
+    // variables to store the previous values of a, b, c, d, and e for the current block
     let prev_a, prev_b, prev_c, prev_d, prev_e;
 
+    // process the input block in chunks of 512 bits (16 32-bit words)
     for (let i = 0; i < block.length; i += 16) {
+
+        // store the previous values of a, b, c, d, and e for this chunk
         [prev_a, prev_b, prev_c, prev_d, prev_e] = [a, b, c, d, e];
 
+        // perform 80 rounds of computation for each chunk
         for (let t = 0; t < 80; t++) {
             w[t] = t < 16 ? block[i + t] :
                 circularLeftShift(w[t - 3] ^ w[t - 8] ^ w[t - 14] ^ w[t - 16], 1);
@@ -61,6 +88,7 @@ function sha1_rounds(block) {
                 )
             );
 
+            // update variables for the next round
             e = d;
             d = c;
             c = circularLeftShift(b, 30);
@@ -68,6 +96,7 @@ function sha1_rounds(block) {
             a = sum;
         }
 
+        // update the values with the values from the previous block
         a = add(a, prev_a);
         b = add(b, prev_b);
         c = add(c, prev_c);
@@ -79,16 +108,19 @@ function sha1_rounds(block) {
 
 }
 
+// This function performs a circular left shift operation on a 32-bit number
 function circularLeftShift(n, k) {
     return (n << k) | (n >>> (32 - k));
 }
 
+// This function adds two 32-bit numbers with overflow handling
 function add(x, y) {
     let l = (x & 0xFFFF) + (y & 0xFFFF),
         h = (x >> 16) + (y >> 16) + (l >> 16);
     return (h << 16) | (l & 0xFFFF);
 }
 
+// This function calculates the 'f' value for a specific round 't' and other block variables
 function fFromT(t, b, c, d) {
     let f;
     if (t >= 0 && t <= 19)
@@ -103,6 +135,7 @@ function fFromT(t, b, c, d) {
     return f;
 }
 
+// This function calculates the 'k' value for a specific round 't'
 function kFromT(t) {
     return t <= 19 ? 0x5A827999 : t <= 39 ? 0x6ED9EBA1 : t <= 59 ? 0x8F1BBCDC : 0xCA62C1D6;
 }
