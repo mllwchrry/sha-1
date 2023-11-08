0fa9a2fdcc9c247382618745cafdcfdf01c9fb00
Mariia Zhvanko <zhvmr09@gmail.com>
2023-11-08 19:04:49 +0200

    sha-1 algorithm implemented

--- sha-1.js
+++ sha-1.js
@@ -1,5 +1,36 @@
 function sha1(message) {
-    return binaryToHex(pad(sha1_rounds(message)));
+    return blocksToHex(sha1_rounds(pad(message)));
+}
+
+function pad(message) {
+    const len = message.length;
+    const blockAmount = Math.ceil((len + 8) / 64);
+    const blocks = new Array(blockAmount * 16).fill(0);
+
+    for (let i = 0; i < len; i++) {
+        const blockIndex = Math.floor(i / 4);
+        const wordIndex = (i % 4) * 8;
+        blocks[blockIndex] |= message.charCodeAt(i) << (24 - wordIndex);
+    }
+
+    blocks[Math.floor(len / 4)] |= 0x80 << (24 - (len % 4) * 8);
+
+    blocks[blockAmount * 16 - 1] = len * 8;
+
+    return blocks;
+}
+
+function blocksToHex(blocks) {
+    let hex = '';
+    for (let i = 0; i < blocks.length; i++) {
+        let block = blocks[i];
+        if (block < 0)
+            block = 0xFFFFFFFF + block + 1;
+
+        hex += block.toString(16);
+    }
+
+    return hex;
 }
 
 function sha1_rounds(block) {
@@ -74,26 +105,4 @@ function kFromT(t) {
     return t <= 19 ? 0x5A827999 : t <= 39 ? 0x6ED9EBA1 : t <= 59 ? 0x8F1BBCDC : 0xCA62C1D6;
 }
 
-function pad(message) {
-    const len = message.length;
-    const blockAmount = Math.ceil((len + 8) / 64);
-    const blocks = new Array(blockAmount * 16).fill(0);
-
-    for (let i = 0; i < len; i++) {
-        const blockIndex = Math.floor(i / 4);
-        const wordIndex = (i % 4) * 8;
-        blocks[blockIndex] |= message.charCodeAt(i) << (24 - wordIndex);
-    }
-
-    blocks[Math.floor(len / 4)] |= 0x80 << (24 - (len % 4) * 8);
-
-    blocks[blockAmount * 16 - 1] = len * 8;
-    console.log(blocks)
-    return blocks;
-}
-
-function binaryToHex(binaryArray) {
-
-}
-
-// console.log(sha1_rounds(pad('hello')))
+// console.log(sha1('test'))
